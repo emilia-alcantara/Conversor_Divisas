df334e1d1c1b4bc384d5e6bc12c934882fae2d19
Emilia <correoalcantaraemilia@gmail.com>
2023-07-20 20:05:35 -0400

    Activación de view binding.

--- app/src/main/java/cl/individual/jueves200723p1/MainActivity.kt
+++ app/src/main/java/cl/individual/jueves200723p1/MainActivity.kt
@@ -2,10 +2,13 @@ package cl.individual.jueves200723p1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.individual.jueves200723p1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
