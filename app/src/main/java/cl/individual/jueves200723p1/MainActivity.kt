5c87a4e45060b45f6177490833d9b616b0a8b2c3
Emilia <correoalcantaraemilia@gmail.com>
2023-07-20 21:26:23 -0400

    Update diseño (aún falta).

--- app/src/main/java/cl/individual/jueves200723p1/MainActivity.kt
+++ app/src/main/java/cl/individual/jueves200723p1/MainActivity.kt
@@ -1,14 +1,35 @@
 package cl.individual.jueves200723p1
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import cl.individual.jueves200723p1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val divisas = listOf<String> ("Dolar", "Pesos", "Euro")
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val spinnerAdapter = ArrayAdapter(this, R.layout.simple_spinner_item, divisas)
+
+        binding.spnMonedaInicial.adapter = spinnerAdapter
+        binding.spnMonedaConvertida.adapter = spinnerAdapter
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnConvertir.setOnClickListener {v ->
+            val montoInicial = binding.editMontoInicial.text.toString().toInt()
+            val divisaActual = binding.spnMonedaInicial.selectedItem.toString()
+            val divisaCambio = binding.spnMonedaConvertida.selectedItem.toString()
+
+
+        }
+
+
     }
 }
